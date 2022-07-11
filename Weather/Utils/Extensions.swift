c9dca1d3c106517fecdc8fe2fae5a5d530b504f9
CarlosDDeLeon <89562671+CarlosDDeLeon@users.noreply.github.com>
2022-07-10 19:31:41 -0700

    Add files via upload

---
+++ Weather/Utils/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  Weather
+//
+//  Created by HELENA BALDERRAMA on 7/4/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = LinearGradient(gradient: Gradient(colors: [Color("Background 1"), Color("Background 2")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+    static let bottomShee
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by CODES WITH COOKIES on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
